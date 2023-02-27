017f43ea543330facfa7ee653856a5ced693f1f1
Lilyaliya <lilyaliya@inbox.ru>
2023-02-28 02:49:37 +0300

    Inventory (p 1) add components

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,7 +7,8 @@ public class Inventory : MonoBehaviour
 {
     [SerializeField] List<GameObject> inventory;
     [SerializeField] int maxSize;
-    [SerializeField] Button element;
+    [SerializeField] RectTransform element;
+    [SerializeField] RectTransform content;
     public void Add(GameObject item)
     {
         if (inventory.Count < maxSize && !inventory.Find(el=> el.name == item.name))
@@ -34,9 +35,17 @@ public class Inventory : MonoBehaviour
     }
     void FillContent(string name)
     {
-        Button newEl = element;
-        newEl.GetComponentInChildren<Text>().text = name;
-        newEl.transform.parent = GameObject.FindWithTag("content").transform;
+        //RectTransform newEl = element;
+        //newEl.GetComponentInChildren<Text>().text = name;
+        //newEl.transform.parent = GameObject.FindWithTag("content").transform;
+        /*foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }*/
+        element.GetComponentInChildren<Text>().text = name;
+        var copy = Instantiate(element.gameObject) as GameObject;
+        copy.transform.SetParent(content, false);
+        content.anchoredPosition = new Vector2(0, -content.rect.width / 2);
         //element.GetComponentInChildren<Text>().text = name;
         //Instantiate(element, GameObject.FindWithTag("content").transform);
     }
